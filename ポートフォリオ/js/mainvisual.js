232653fdcea6da0f4859a7d017c49e095808b726
gorouzaemon <stngdss051@outlook.jp>
2024-04-09 14:43:06 +0900

    hamburger

--- ポートフォリオ/js/mainvisual.js
+++ ポートフォリオ/js/mainvisual.js
@@ -1,14 +1,12 @@
-//  hamburgerのjs
-$(document).on('click','.openbtn',function () {//ボタンがクリックされたら
+$(".openbtn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
 });
 
-$("#g-nav a,.mainvisual").click(function () {//ナビゲーションのリンクがクリックされたら
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
-
 
 // goemonの棒伸びるやつ
 $(function(){
